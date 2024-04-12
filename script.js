2d4ff6ca721985332a43667f2eebc53106455e0d
Marta Szulc <marta.szulc2202@gmail.com>
2024-04-12 15:27:26 +0200

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-fetch('https://whatyearisit-backend-jet-phi.vercel.app')
+fetch('https://whatyearisit-backend-jet-phi.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#year').textContent = data.year;
+        console.log(data);
     })
